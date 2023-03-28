aef4ddf0e51a608adde8f7dd05e3ff5aee63415c
22 <60903333+nini22P@users.noreply.github.com>
2023-03-29 00:35:36 +0800

    fix layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'
 const Header = ({ search, tagsNum, filteredNum }) => {
   return (
     <div className='header'>
-      <a href='https://github.com/nini22P/waifu-tags-cn'>Waifu Tags CN</a>
+      {/* <a href='https://github.com/nini22P/waifu-tags-cn'>GitHub</a> */}
       <input type='search' className='search' placeholder='搜索' onChange={search} />
       <div className='tagNum'>{filteredNum} / {tagsNum}</div>
     </div>
